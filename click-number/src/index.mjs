b2ba2099c940c37dab130dac46ed5902106f3375
LAPTOP-PIBQA2PF\user <slimehirotk2@icloud.com>
2024-05-29 15:56:04 +0900

    丸生成時にランダムな色になるよう変更。

--- click-number/src/index.mjs
+++ click-number/src/index.mjs
@@ -1,47 +1,38 @@
 import "./styles.css";
 
-const canvas = document.querySelector("#draw-area");
-const context = canvas.getContext("2d");
-context.strokeStyle = "blue";
-canvas.addEventListener("mousemove", (event) => {
-  draw(event.layerX, event.layerY);
-});
-canvas.addEventListener("touchmove", (event) => {
-  draw(event.layerX, event.layerY);
-});
-
-canvas.addEventListener("mousedown", () => {
-  context.beginPath();
-  isDrag = true;
-});
-
-canvas.addEventListener("mouseup", () => {
-  context.closePath();
-  isDrag = false;
-});
-
-canvas.addEventListener("touchstart", () => {
-  context.beginPath();
-  isDrag = true;
-});
-
-canvas.addEventListener("touchend", () => {
-  context.closePath();
-  isDrag = false;
-});
-
-const clearButton = document.querySelector("#clear-button");
-clearButton.addEventListener("click", () => {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-});
-
-let isDrag = false;
-
-function draw(x, y) {
-  if (!isDrag) {
-    return;
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-  context.lineWidth = 5;
-  context.lineTo(x, y);
-  context.stroke();
+  return color;
 }
+
+for (var num = 9; num > 0; num--) {
+  var elm = document.createElement("button");
+  elm.innerHTML = num;
+  elm.setAttribute("id", num);
+  elm.setAttribute("class", "circle");
+  var function_name = "remove(" + num + ")";
+  elm.setAttribute("onclick", function_name);
+  document.getElementById("main").appendChild(elm);
+
+  var left_pos = 10;
+  var top_pos = 100;
+
+  left_pos = left_pos + Math.floor(Math.random() * 400);
+  top_pos = top_pos + Math.floor(Math.random() * 600);
+
+  document.getElementById(num).style.left = "" + left_pos + "px";
+  document.getElementById(num).style.top = "" + top_pos + "px";
+  document.getElementById(num).style.backgroundColor = getRandomColor();
+}
+
+var next = 1;
+document.remove = function (id) {
+  if (id === next) {
+    document.getElementById("main").removeChild(document.getElementById(id));
+    next = next + 1;
+  }
+};
